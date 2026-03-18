362cc16450966b91c8d729442c8c6f61d6ced27a
0x.acessoria <aecioemailnet@gmail.com>
2026-03-17 23:08:18 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'espaco-pro-v2';
-const assets = ['/', '/index.html', '/manifest.json'];
+const assets = ['index.html', 'manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
